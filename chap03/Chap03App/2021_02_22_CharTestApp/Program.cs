6472b8e3aaf2e2df116fcdac492d794ef0531100
SeoDongWoo1216 <dw6642@naver.com>
2021-02-22 12:32:48 +0900

    변수 study

---
+++ chap03/Chap03App/2021_02_22_CharTestApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_02_22_CharTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 문자
+            Console.WriteLine("-----------------------------문자------------------------------");
+            // char ch9 = 'test' 은 오류인데, ''는 문자 1개만 저장할 수 있기때문. (글자를 여러개쓰면 '문자열'(string)이 된다.)
+            char ch1 = 't' ,   // 일반적인 글자
+                 ch2 = '\t',   // Tab 효과
+                 ch3 = '\n',   // 엔터효과
+                 ch4 = '0';    // 숫자 0 저장
+            Console.WriteLine($"{ch1}, {ch2}, {ch3}, {ch4}");
+
+            char ch5 = '\\'; // 역슬래시 출력하려면 2개를 입력해야함.
+            Console.WriteLine($"{ch5}");
+            Console.WriteLine();
+
+            // char ch9 = 'test' 은 오류인데, ''는 문자 1개만 저장할 수 있기때문. (글자를 여러개쓰면 '문자열'(string)이 된다.)
+
+
+            // 문자열
+            Console.WriteLine("-----------------------------문자열-----------------------------");
+            string str1 = "Hello, World!";
+            // Console.WriteLine(str1);
+            Console.WriteLine($"{str1}");
+
+            string str2 = "\tHello, \nWorld!";  // 이스케이프 시퀀스 사용
+            Console.WriteLine($"{str2}");
+            Console.WriteLine();
+
+
+            // 불린형
+            Console.WriteLine("-----------------------------불린형-----------------------------");
+            bool isCorrect = false;
+            Console.WriteLine(isCorrect);
+            if (isCorrect)
+            {
+                Console.WriteLine("참입니다.");
+            }
+            else
+            {
+                Console.WriteLine("거짓입니다.");
+            }
+        }
+    }
+}
---
+++ chap03/Chap03App/VariableTestApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace VariableTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int iv1 = 30, iv2 = 40;
+            //int result = iv1 + iv2;
+            //Console.WriteLine(result);
+
+            sbyte sbMinval = sbyte.MinValue;
+            sbyte sbMaxval = sbyte.MaxValue;
+            //Console.WriteLine("{0}, {1}", sbMinval, sbMaxval);
+            Console.WriteLine($"Sbyte  최소, 최대값은 {sbMinval}, {sbMaxval} 입니다.");
+
+            short shMinval = short.MinValue;
+            short shMaxval = short.MaxValue;
+            Console.WriteLine($"short  최소, 최대값은 {shMinval}, {shMaxval} 입니다.");
+
+            ushort ushMinval = ushort.MinValue;
+            ushort ushMaxval = ushort.MaxValue;
+            Console.WriteLine($"ushort 최소, 최대값은 {ushMinval}, {ushMaxval} 입니다.");
+
+            int iMinval = int.MinValue;
+            int iMaxval = int.MaxValue;
+            Console.WriteLine($"int    최소, 최대값은 {iMinval}, {iMaxval} 입니다.");
+
+            uint uiMinval = uint.MinValue;
+            uint uiMaxval = uint.MaxValue;
+            Console.WriteLine($"uint   최소, 최대값은 {uiMinval}, {uiMaxval} 입니다.");
+
+            long lMinval = long.MinValue;
+            long lMaxval = long.MaxValue;
+            Console.WriteLine($"long   최소, 최대값은 {lMinval}, {lMaxval} 입니다.");
+
+            ulong ulMinval = ulong.MinValue;
+            ulong ulMaxval = ulong.MaxValue;
+            Console.WriteLine($"ulong  최소, 최대값은 {ulMinval}, {ulMaxval} 입니다.");
+
+
+
+        }
+    }
+}
