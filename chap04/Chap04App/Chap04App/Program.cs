b8d2e0342242407053303374781f6d1845616f6a
SeoDongWoo1216 <dw6642@naver.com>
2021-02-22 17:27:29 +0900

    4장 커밋

---
+++ chap04/Chap04App/Chap04App/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap04App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 나머지 연산자 '%' 예제
+
+            var values = 37656234;
+
+            if(values % 2 == 0)
+            {
+                Console.WriteLine($"{values}는 짝수입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{values}는 아무것도 아닙니다.");
+            }
+
+            if (values % 3 == 0)
+            {
+                Console.WriteLine($"{values}는 3의 배수입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{values}는 3의 배수가 아닙니다..");
+            }
+
+            if (values % 7 == 0)
+            {
+                Console.WriteLine($"{values}는 7의 배수입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{values}는 7의 배수가 아닙니다..");
+            }
+
+
+        }
+    }
+}
---
+++ chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result} 입니다.");
+
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result} 입니다.");
+
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result} 입니다.");
+            Console.WriteLine();
+
+            Console.WriteLine($"후치증감 값은 {result++} 입니다."); // 후치연산 : 이 줄을 실행하고, 연산자를 연산함.(출력은 19, 값은 20으로 저장)
+            Console.WriteLine($"전치증감 값은 {++result} 입니다."); // 1증가시킨 후에 출력 
+
+        }
+    }
+}
