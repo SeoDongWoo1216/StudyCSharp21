48c4acd7512440c497972d9c88009afefdd98cea
SeoDongWoo1216 <dw6642@naver.com>
2021-03-03 12:39:00 +0900

    13장 이벤트

---
+++ chap13/chap13App/21_03_03_01_DelegateChain2App(Fire)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_03_03_01_DelegateChain2App_Fire_
+{
+    class Program
+    {
+        delegate void ThereIsAFire(string location);   // 대리자 선언
+
+        static void Call119(string location)
+        {
+            Console.WriteLine($"소방서죠? 불이 났어요! 주소는 {location} 입니다!");
+        }
+
+        static void ShoutOut(string location)
+        {
+            Console.WriteLine($"불이야! {location} 에 불났어요!");
+        }
+
+        static void Escape(string location)
+        {
+            Console.WriteLine($"{location}에서 나갑시다!!");
+        }
+
+
+        static void Main(string[] args)
+        {
+            ThereIsAFire fire = new ThereIsAFire(ShoutOut);
+            fire += new ThereIsAFire(Call119);    // 표준 문법
+            fire += Escape;                       // 약식 문법
+
+            fire("문현동 전광빌라");
+            Console.WriteLine();
+            fire("부산시 어딘가");
+
+      
+        }
+    }
+}
---
+++ chap13/chap13App/21_03_03_02_UsingEventApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _21_03_03_02_UsingEventApp
+{
+    delegate void EventHandler(string message); // 메세지를 받아서 처리하는 대리자 선언
+
+    class CustomNotifier
+    {
+        // 이벤트 선언, 사용하는 객체
+        public event EventHandler SomethingHappend;   // 대리자의 인스턴스 이벤트
+
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;  
+
+            if(temp != 0 && temp % 3 == 0)  // 3, 6, 9로 떨어지는 값
+            {
+                SomethingHappend($"{number} : 짝!");  // 이벤트를 사용(로직은 없음)
+            }
+            else
+            {
+                SomethingHappend($"{number}");   // 이벤트를 사용
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        public static void MyHandler(string message)  // 이벤트가 발생했을때 실행되는 메서드(이벤트 핸들러)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이벤트 사용!");
+            CustomNotifier notifier = new CustomNotifier();
+            notifier.SomethingHappend += new EventHandler(MyHandler);   // 이벤트를 내가 만든 로직이 있는 메서드랑 연결
+
+            for (int i = 1; i <= 100; i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+    }
+}
